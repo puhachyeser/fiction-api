86919dfe0ec10663b0c1d26273a0b5cf33567749
puhachyeser <yehorpuhach@gmail.com>
2025-04-09 10:43:47 +0300

    general Fiction model added and discriminators to it, also functionality on fiction controller changed accordingly

--- controllers/fictions.js
+++ controllers/fictions.js
@@ -1,19 +1,37 @@
-const Book = require('../models/book')
-//const Game = require('../models/game')
-//const Movie = require('../models/movie')
+const Fiction = require('../models/Fiction')
 const { StatusCodes } = require('http-status-codes')
 //const { BadRequestError, NotFoundError } = require('../errors')
 
+const getModelType = (req) => {
+    const { type } = req.body;
+    let Model;
+
+    switch (type) {
+    case 'book':
+        Model = Fiction.discriminators.book;
+        break;
+    case 'movie':
+        Model = Fiction.discriminators.movie;
+        break;
+    case 'game':
+        Model = Fiction.discriminators.game;
+        break;
+    default:
+        return res.status(StatusCodes.BAD_REQUEST).json({ msg: 'Invalid type' });
+    }
+    return Model;
+}
+
 const getAllFictions = async (req, res) => {
     //const fictions = await Book.find({ createdBy: req.user.userId }).sort('createdAt')
-    const fictions = await Book.find().sort('createdAt')
+    const fictions = await Fiction.find().sort('createdAt')
     res.status(StatusCodes.OK).json({ fictions, count: fictions.length })
 }
 
 const getFiction = async (req, res) => {
     const { params: { id: fictionId }} = req
 
-    const fiction = await Book.findOne({_id: fictionId})
+    const fiction = await Fiction.findOne({_id: fictionId})
     if (!fiction) {
         res.status(StatusCodes.NOT_FOUND).json("not found error")
         //throw new NotFoundError(`No fiction with id ${fictionId}`)
@@ -23,15 +41,16 @@ const getFiction = async (req, res) => {
 
 const createFiction = async (req, res) => {
     //req.body.createdBy = req.user.userId    after auth
-    const fiction = await Book.create(req.body)
+    let Model = getModelType(req)
+    const fiction = await Model.create(req.body)
     res.status(StatusCodes.CREATED).json({ fiction })
 }
 
 const updateFiction = async (req, res) => {
     const { params: { id: fictionId }} = req
-    // TODO: discriminator for different type of models. All models in one file Fiction.js
+    let Model = getModelType(req)
 
-    const fiction = await Book.findOneAndUpdate({_id: fictionId}, req.body, { new: true, runValidators: true })
+    const fiction = await Model.findOneAndUpdate({_id: fictionId}, req.body, { new: true, runValidators: true })
     if (!fiction) {
         res.status(StatusCodes.NOT_FOUND).json("not found error")
         //throw new NotFoundError(`No fiction with id ${fictionId}`)
@@ -42,7 +61,7 @@ const updateFiction = async (req, res) => {
 const deleteFiction = async (req, res) => {
     const { params: { id: fictionId }} = req
 
-    const fiction = await Book.findOneAndDelete({_id: fictionId})
+    const fiction = await Fiction.findOneAndDelete({_id: fictionId})
     if (!fiction) {
         res.status(StatusCodes.NOT_FOUND).json("not found error")
         //throw new NotFoundError(`No fiction with id ${fictionId}`)
---
+++ models/Fiction.js
@@ -0,0 +1,99 @@
+const { date } = require('joi')
+const mongoose = require('mongoose')
+
+const BaseFictionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please provide title'],
+        maxlength: 30,
+    },
+    description: {
+        type: String,
+        required: [true, 'Please provide description'],
+        maxlength: 100
+    },
+    releaseYear: {
+        type: Number,
+        required: [true, 'Please provide release year'],
+        maxlength: 4,
+        validate: /^[12][0-9]{3}$/
+    }
+    }, { timestamps: true, discriminatorKey: 'type', collection: 'fictions' });
+  
+const Fiction = mongoose.model('Fiction', BaseFictionSchema);
+
+const Book = Fiction.discriminator('book', new mongoose.Schema({
+    author: {
+        type: String,
+        required: [true, 'Please provide author'],
+        maxlength: 30,
+    },
+    pages: {
+        type: Number,
+        required: [true, 'Please provide number of pages'],
+        maxlength: 3,
+    }
+}));
+
+const Movie = Fiction.discriminator('movie', new mongoose.Schema({
+    director: {
+        type: String,
+        required: [true, 'Please provide director'],
+        maxlength: 30,
+    },
+    duration: {
+        type: String,
+        required: [true, 'Please provide number of pages'],
+        maxlength: 10,
+        validate: /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
+    }
+}));
+
+const Game = Fiction.discriminator('game', new mongoose.Schema({
+    studio: {
+        type: String,
+        required: [true, 'Please provide studio'],
+        maxlength: 30,
+    },
+    timeToBeat: {
+        type: String,
+        required: [true, 'Please provide time to beat'],
+        maxlength: 10,
+        validate: /^\d+(\.\d+)?h$/
+    }
+}));
+
+/*
+const BookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide title'],
+      maxlength: 30,
+    },
+    author: {
+      type: String,
+      required: [true, 'Please provide author'],
+      maxlength: 30,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide description'],
+      maxlength: 100
+    },
+    realeaseYear: {
+        type: Number,
+        required: [true, 'Please realease year'],
+        minLength: 4,
+        maxLength: 4
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+)
+*/
+module.exports = mongoose.model('Fiction', BaseFictionSchema)
