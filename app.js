e8eab8b85d0866ce0290370d93606761fe038b67
puhachyeser <yehorpuhach@gmail.com>
2025-04-14 09:25:30 +0300

    security packages fix

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require("express")
 const app = express()
 
