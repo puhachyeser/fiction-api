3abe77fa181d4eaa8412da0a58037958ee0d639d
puhachyeser <yehorpuhach@gmail.com>
2025-04-17 09:21:45 +0300

    swagger ui initial setup

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const helmet = require('helmet');
-const cors = require('cors');
-const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet')
+const cors = require('cors')
+const rateLimiter = require('express-rate-limit')
 
 const express = require("express")
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./docs.yaml')
 const app = express()
 
-const connectDB = require('./db/connect');
-const authenticateUser = require('./middleware/authentication');
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
 
-const authRouter = require('./routes/auth');
-const fictionsRouter = require('./routes/fictions');
+const authRouter = require('./routes/auth')
+const fictionsRouter = require('./routes/fictions')
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.set('trust proxy', 1);
+app.set('trust proxy', 1)
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000,
@@ -24,22 +27,26 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("Main")
+    res.send(`
+      <h1>API Documentation</h1>
+      <a href="/api-docs">Documentation</a>
+      `)
 })
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.use('/fictions-api/v1/auth', authRouter);
-app.use('/fictions-api/v1/fictions', authenticateUser, fictionsRouter);
+app.use('/fictions-api/v1/auth', authRouter)
+app.use('/fictions-api/v1/fictions', authenticateUser, fictionsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
